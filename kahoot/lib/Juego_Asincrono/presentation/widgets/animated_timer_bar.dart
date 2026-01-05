a3f1a6616e041f1fcf0125651695bb13a0c170fb
Nicola Manfredi <ncamanfredi@gmail.com>
2026-01-05 12:25:14 -0400

    Eliminación de código comentado en GamePage y AnimatedTimerBar para mejorar la legibilidad

--- kahoot/lib/Juego_Asincrono/presentation/widgets/animated_timer_bar.dart
+++ kahoot/lib/Juego_Asincrono/presentation/widgets/animated_timer_bar.dart
@@ -1,41 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// class AnimatedTimerBar extends StatelessWidget {
-//   final double progress;
-//   const AnimatedTimerBar({super.key, required this.progress});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       width: double.infinity,
-//       height: 10,
-//       decoration: BoxDecoration(
-//         color: Colors.white10,
-//         boxShadow: [
-//           BoxShadow(
-//             color: Colors.black.withOpacity(0.1),
-//             blurRadius: 4,
-//             offset: const Offset(0, 2),
-//           ),
-//         ],
-//       ),
-//       child: FractionallySizedBox(
-//         alignment: Alignment.centerLeft,
-//         widthFactor: progress.clamp(0.0, 1.0),
-//         child: Container(
-//           decoration: const BoxDecoration(
-//             color: Colors.white,
-//             borderRadius: BorderRadius.only(
-//               topRight: Radius.circular(5),
-//               bottomRight: Radius.circular(5),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import '../utils/game_constants.dart';
 
@@ -56,7 +18,7 @@ class AnimatedTimerBar extends StatelessWidget {
         widthFactor: progress.clamp(0.0, 1.0),
         child: Container(
           decoration: const BoxDecoration(
-            color: GameColors.amberTheme, // AQUI EL CAMBIO
+            color: GameColors.amberTheme, 
             borderRadius: BorderRadius.horizontal(right: Radius.circular(5)),
           ),
         ),
