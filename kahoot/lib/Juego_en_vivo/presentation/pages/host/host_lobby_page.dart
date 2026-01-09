befddd4ce3849980155fdd74ef8aefac3416c5d6
JuanJoseCammarano <jjcammarano.22@est.ucab.edu.ve>
2026-01-09 12:10:25 -0400

    Actualización del token para probar

--- kahoot/lib/Juego_en_vivo/presentation/pages/host/host_lobby_page.dart
+++ kahoot/lib/Juego_en_vivo/presentation/pages/host/host_lobby_page.dart
@@ -29,7 +29,7 @@ class _HostLobbyViewState extends State<HostLobbyView> {
         baseUrl: 'https://quizzy-backend-0wh2.onrender.com/api',
         headers: {
           'Authorization':
-              'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjdhYmM2ZmVkLTY2NWUtNDYzZC1iNTRkLThkNzhjMTM5N2U2ZiIsImVtYWlsIjoibmNhcmxvc0BleGFtcGxlLmNvbSIsInJvbGVzIjpbInVzZXIiXSwiaWF0IjoxNzY3OTY3NjAyLCJleHAiOjE3Njc5NzQ4MDJ9.4VcrTgTw8bo-bzdZph2fTtlJEP9lCIcBwE5H6K9647w',
+              'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjdhYmM2ZmVkLTY2NWUtNDYzZC1iNTRkLThkNzhjMTM5N2U2ZiIsImVtYWlsIjoibmNhcmxvc0BleGFtcGxlLmNvbSIsInJvbGVzIjpbInVzZXIiXSwiaWF0IjoxNzY3OTc0OTM2LCJleHAiOjE3Njc5ODIxMzZ9.9IAS2IteLAcGeeGUYbTvkYQPAIFZae5Wam1Nbz32ZtU',
           'Content-Type': 'application/json',
         },
       ),
